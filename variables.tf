9fee836f8ee1b85d1a265e6270e8b05ae239db80
amarouane <amarouane@itsc.uah.edu>
2020-05-08 12:41:00 -0500

    Change variables

--- variables.tf
+++ variables.tf
@@ -29,7 +29,7 @@ variable "cluster_arn" {}
 variable "log2elasticsearch_lambda_function_arn" {}
 variable "environement" {
   default = {
-    AWS_DEFAULT_REGION = var.region
+    AWS_DEFAULT_REGION = "us-west-2"
   }
 }
 variable "docker_image" {
