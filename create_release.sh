1375ee168cbca9c6c85b0051354ba3a37b772d56
amarouane <amarouane@itsc.uah.edu>
2020-11-05 12:51:16 -0600

    Add automated release

---
+++ create_release.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -ex
+export VERSION=$1
+export RELEASE_NAME=`basename $GITHUB_REPO`
+
+## Create Release
+   export RELEASE_URL=$(curl -H\
+  "Authorization: token $SECRET_TOKEN"\
+   -d "{\"tag_name\": \"v$VERSION\", \"target_commitsh\": \"v$VERSION\", \"name\": \"v$VERSION\", \"body\": \"Release v$VERSION\" }"\
+   -H "Content-Type: application/json"\
+   -X POST\
+   https://api.github.com/repos/$GITHUB_REPO/releases |grep \"url\" |grep releases |sed -e 's/.*\(https.*\)\"\,/\1/'| sed -e 's/api/uploads/')
+
+## Build TF modules that require source building
+function create_zip_file() {
+
+  BUILD_DIR=/tmp/${RELEASE_NAME}
+  DESTINATION_DIR=${PWD}/dist
+  rm -rf ${DESTINATION_DIR}
+  mkdir -p ${BUILD_DIR} ${DESTINATION_DIR}
+  cp -r modules ${BUILD_DIR}
+  cp *tf ${BUILD_DIR}
+  cd ${BUILD_DIR}
+  zip -r9 ${RELEASE_NAME}.zip .
+  mv ${RELEASE_NAME}.zip ${DESTINATION_DIR}/.
+  cd $DESTINATION_DIR
+  rm -rf ${BUILD_DIR}
+
+
+}
+#### Release package
+create_zip_file
+
+### Post the release
+curl -X POST -H "Authorization: token $SECRET_TOKEN" --data-binary "@${RELEASE_NAME}.zip" -H "Content-type: application/octet-stream" $RELEASE_URL/assets?name=${RELEASE_NAME}.zip
---
+++ env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export GITHUB_TOKEN=cb3b62ba2fe50f1ebd4c9b418792c29df3b13da9
+export GITHUB_REPO=ghrcdaac/dmrpp-generator
