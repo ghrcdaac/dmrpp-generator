ce2327a541c42825e529c0b5da72abd29e9726b5
amarouane <amarouane@itsc.uah.edu>
2021-03-25 13:52:01 -0500

    Include missing files to dmrpp generator

--- tests/test_dmrpp_command_line.py
+++ tests/test_dmrpp_command_line.py
@@ -1,8 +1,7 @@
 import os
 from unittest import TestCase
-import json
 
-from dmrpp_generator.dmrpp_command_line import DMRPPCommandLine
+from dmrpp_generator.main import DMRPPGenerator
 
 class TestDMRPPCommandLine(TestCase):
 
@@ -11,8 +10,7 @@ class TestDMRPPCommandLine(TestCase):
         Testing local no env var
         :return:
         """
-        dmrpp = DMRPPCommandLine()
-        self.assertEqual('get_dmrpp -b foo -o bar.dmrpp bar', dmrpp.get_command(None, 'foo', 'bar'))
+        self.assertEqual('get_dmrpp -b foo -o bar.dmrpp bar', DMRPPGenerator.get_dmrpp_command({}, 'foo', 'bar'))
 
     def test_2_local_m(self):
         """
@@ -20,8 +18,7 @@ class TestDMRPPCommandLine(TestCase):
         :return:
         """
         os.environ['CREATE_MISSING_CF'] = 'true'
-        dmrpp = DMRPPCommandLine()
-        self.assertEqual('get_dmrpp -M -b foo -o bar.dmrpp bar', dmrpp.get_command(None, 'foo', 'bar'))
+        self.assertEqual('get_dmrpp -M -b foo -o bar.dmrpp bar', DMRPPGenerator.get_dmrpp_command({}, 'foo', 'bar'))
 
     def test_3_local_no_m(self):
         """
@@ -29,8 +26,7 @@ class TestDMRPPCommandLine(TestCase):
         :return:
         """
         os.environ['CREATE_MISSING_CF'] = 'false'
-        dmrpp = DMRPPCommandLine()
-        self.assertEqual('get_dmrpp -b foo -o bar.dmrpp bar', dmrpp.get_command(None, 'foo', 'bar'))
+        self.assertEqual('get_dmrpp -b foo -o bar.dmrpp bar', DMRPPGenerator.get_dmrpp_command({}, 'foo', 'bar'))
 
     def test_4_local_m(self):
         """
@@ -38,95 +34,25 @@ class TestDMRPPCommandLine(TestCase):
         :return:
         """
         os.environ['CREATE_MISSING_CF'] = '1'
-        dmrpp = DMRPPCommandLine()
-        self.assertEqual('get_dmrpp -M -b foo -o bar.dmrpp bar', dmrpp.get_command(None, 'foo', 'bar'))
+        self.assertEqual('get_dmrpp -M -b foo -o bar.dmrpp bar', DMRPPGenerator.get_dmrpp_command({}, 'foo', 'bar'))
 
     def test_5_cumulus_no_meta_config(self):
         """
         Testing cumulus no config 1
         :return:
         """
+        os.environ['CREATE_MISSING_CF'] = '0'
 
-        config = """
-        {
-            "config": {
-                
-            }
-        }
-        """
-        dmrpp = DMRPPCommandLine()
-        self.assertEqual('get_dmrpp -b foo -o bar.dmrpp bar', dmrpp.get_command(config, 'foo', 'bar'))
+        self.assertEqual('get_dmrpp -b foo -o bar.dmrpp bar', DMRPPGenerator.get_dmrpp_command({}, 'foo', 'bar'))
 
-    def test_6_cumulus_no_dmrpp_config(self):
-        """
-        Testing cumulus no config 1
-        :return:
-        """
 
-        config = """
-        {
-            "config": {
-                "meta": {
-                }              
-            }
-        }
-        """
-        dmrpp = DMRPPCommandLine()
-        self.assertEqual('get_dmrpp -b foo -o bar.dmrpp bar', dmrpp.get_command(config, 'foo', 'bar'))
-
-    def test_7_cumulus_m(self):
+    def test_6_cumulus_m(self):
         """
         Testing cumulus true
         :return:
         """
-        config = """
-        {
-            "config": {
-                "meta": {
-                    "dmrpp": {
-                        "create_missing_cf" : true    
-                    }
-                }
-            }
+        os.environ['CREATE_MISSING_CF'] = '0'
+        dmrpp_meta = {
+            "create_missing_cf": "true"
         }
-        """
-        dmrpp = DMRPPCommandLine()
-        self.assertEqual('get_dmrpp -M -b foo -o bar.dmrpp bar', dmrpp.get_command(config, 'foo', 'bar'))
-
-    def test_8_cumulus_m_false(self):
-        """
-        Testing cumulus false
-        :return:
-        """
-        config = """
-        {
-            "config": {
-                "meta": {
-                    "dmrpp": {
-                        "create_missing_cf" : false    
-                    }
-                }
-            }
-        }
-        """
-        dmrpp = DMRPPCommandLine()
-        self.assertEqual('get_dmrpp -b foo -o bar.dmrpp bar', dmrpp.get_command(config, 'foo', 'bar'))
-    
-    def test_9_cumulus_no_m(self):
-        """
-        Testing cumulus no explicit config
-        :return:
-        """
-        config = """
-        {
-            "config": {
-                "meta": {
-                    "dmrpp": {
-                    }
-                }
-            }
-        }
-        """
-        dmrpp = DMRPPCommandLine()
-        self.assertEqual('get_dmrpp -b foo -o bar.dmrpp bar', dmrpp.get_command(config, 'foo', 'bar'))
-
+        self.assertEqual('get_dmrpp -M -b foo -o bar.dmrpp bar', DMRPPGenerator.get_dmrpp_command(dmrpp_meta, 'foo', 'bar'))
