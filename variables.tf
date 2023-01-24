340a2715b44bc70976892f8efb5d13cf59bcee4c
Michael Hall <mlh0079@uah.edu>
2023-01-24 17:05:33 -0600

    - Corrected typo of the besd version.

--- variables.tf
+++ variables.tf
@@ -34,7 +34,8 @@ variable "log_destination_arn" {
 variable "docker_image" {
     description = "Docker image used to generate DMRPP"
     type    = string
-    default = "ghrcdaac/dmrpp-generator:VERSION_SUB"
+//    default = "ghrcdaac/dmrpp-generator:VERSION_SUB"
+    default = "michaellh/dmrpp-testing:latest"
 }
 
 variable "volumes" {
