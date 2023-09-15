8b494072a4a5169fa72118d7eb59b671424ef63b
Michael Hall <mlh0079@uah.edu>
2023-09-15 12:57:28 -0500

    - Removed fake progress bar, refactored the code in main to be more reasonable, added flags for validation or not, and improved formatting.

--- dmrpp_generator/generate_and_validate_dmrpp.py
+++ dmrpp_generator/generate_and_validate_dmrpp.py
@@ -1,57 +1,29 @@
 #! /usr/bin/python3
 import argparse
+import json
 import subprocess
-import time
 import os
 from multiprocessing import Process
 import tempfile
 
 
-def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, separate_bar='-', length=100, fill='█',
-                       print_end="\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        separate_bar - Optional : what will separate the bar as it fills
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filled_length = int(length * iteration // total)
-    f_bar = fill * filled_length + separate_bar * (length - filled_length)
-    print(f'\r{prefix} |{f_bar}| {percent}% {suffix}', end=print_end)
-    # Print New Line on Complete
-    if iteration == total:
-        print()
-
-
 def generate_docker_compose():
     _, dockercompose_file_location = tempfile.mkstemp(suffix=".yml")
-    with open(dockercompose_file_location,'w', encoding="utf-8") as dockercompose_file:
+    with open(dockercompose_file_location, 'w', encoding="utf-8") as dockercompose_file:
         dockercompose_file.write(
             """
 version: '3'
 services:
   dmrpp:
-    # Path to dockerfile.
-    # '.' represents the current directory in which
-    # docker-compose.yml is present.
-    image: ghrcdaac/dmrpp-generator:v4.1.1
+    image: mlh/dmrpp:latest
     environment:
       - PAYLOAD=${PAYLOAD}
+      - DMRPP_ARGS=${DMRPP_ARGS}
     # Mount volume
     volumes:
       - ${NC_FILES_PATH:-/tmp}:/usr/share/hyrax
 
   hyrax:
-
-    # image to fetch from docker hub
     image: opendap/hyrax:snapshot
     ports:
     - "${PORT:-8080}:8080"
@@ -59,23 +31,9 @@ services:
       - ${NC_FILES_PATH:-/tmp}:/usr/share/hyrax/
     working_dir: /usr/share/hyrax
     container_name: hyrax
-
             """
         )
     return dockercompose_file_location
-
-
-def progress_bar(file_number, prefix='Generating:', suffix='Complete', length=50, fill='█', separate_bar='-'):
-    items = list(range(0, min(file_number * 25, 600)))
-    items_length = len(items)
-    # Initial call to print 0% progress
-    print_progress_bar(iteration=0, total=items_length, prefix=prefix, suffix=suffix, length=length, fill=fill,
-                       separate_bar=separate_bar)
-    for i, _ in enumerate(items):
-        time.sleep(0.1)
-        # Update Progress Bar
-        print_progress_bar(iteration=i + 1, total=items_length, prefix=prefix, suffix=suffix, length=length, fill=fill,
-                           separate_bar=separate_bar)
 
 
 def check_docker_version(log_file_path):
@@ -90,64 +48,71 @@ def check_docker_version(log_file_path):
     return dkr_comp_version
 
 
-def run_docker_compose(payload, nc_hdf_path, port, dmrrpp_service, log_file_path):
+def run_docker_compose(payload, dmrpp_args, nc_hdf_path, port, dmrrpp_service, log_file_path):
     dockercompose_file_location = generate_docker_compose()
     dkr_comp_version = check_docker_version(log_file_path)
 
     with open(log_file_path, "a", encoding='utf-8') as output:
         try:
-            cmd = f"PAYLOAD='{payload}' NC_FILES_PATH={nc_hdf_path} PORT={port} {dkr_comp_version} " \
-                    f"-f {dockercompose_file_location} up {dmrrpp_service}"
+            cmd = f"PAYLOAD='{payload}' " \
+                  f"DMRPP_ARGS='{dmrpp_args}' " \
+                  f"NC_FILES_PATH={nc_hdf_path} " \
+                  f"PORT={port} {dkr_comp_version} " \
+                  f"-f {dockercompose_file_location} up {dmrrpp_service}"
             subprocess.run(
                 cmd,
                 shell=True, check=False, stdout=output,
                 stderr=output)
         except KeyboardInterrupt:
             cmd = f" {dkr_comp_version} -f {dockercompose_file_location} down {dmrrpp_service}"
-            subprocess.run(cmd, shell=True, check=False,
-                            stdout=output,
-                            stderr=output)
+            subprocess.run(cmd, shell=True, check=False, stdout=output, stderr=output)
             os.remove(dockercompose_file_location)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate and validate DMRPP files.')
-    parser.add_argument('-p', '--path', dest='nc_hdf_path', nargs=1, required=True,
+    parser = argparse.ArgumentParser(
+        description='Generate and validate DMRPP files. Any DMR++ commandline option can be provided in addition to'
+                    ' the options listed below. To see what options are available check the documentation: '
+                    'https://docs.opendap.org/index.php?title=DMR%2B%2B#Command_line_options'
+    )
+    parser.add_argument('-p', '--path', dest='nc_hdf_path', required=True,
                         help='Path to netCDF4 and HDF5 folder')
-    parser.add_argument('-prt', '--port', dest='port', nargs=1, default=["8080"],
+    parser.add_argument('-prt', '--port', dest='port', default="8080",
                         help='Port number to Hyrax local server')
-    parser.add_argument('-pyld', '--payload', dest='payload', nargs=1, default=['{}'],
-                        help='Payload to execute get_dmrpp binary')
-    parser.add_argument('-vldt', '--validate', dest='validate', nargs=1, default=['true'],
-                        help='Validate netCDF4 and HDF5 files against OPeNDAP local server')
+    parser.add_argument('-pyld', '--payload', dest='payload', default=os.getenv('PAYLOAD', '{}'),
+                        help='Payload to pass to the besd get_dmrpp call. '
+                             'If not set, will check for PAYLOAD environment variable, or default to \'{}\'')
+    parser.add_argument('--validate', action='store_true',
+                        help='Validate netCDF4 and HDF5 files against OPeNDAP local server. '
+                             'This is the default behavior')
+    parser.add_argument('--no-validate', dest='validate', action='store_false',
+                        help='Do not validate netCDF4 and HDF5 files against OPeNDAP local server. '
+                             'The default behavior is --validate.')
+    parser.set_defaults(validate=True)
 
-    args = parser.parse_args()
-    nc_hdf_path, port, payload, validate = [getattr(args, ele)[0] for ele in args.__dict__.keys()]
-    no_need_validation = validate not in ['true', '1', 'yes', 'y']
-    # If the user doesn't want validation run dmrpp service alone without Hyrax UI
-    dmrrpp_service = "dmrpp" if no_need_validation else ""
-    # Depending on needing the validation the user should get either a path or Hyrax UI link
-    visit_link_path_message = f"{nc_hdf_path}" if no_need_validation else f"http://localhost:{port}/opendap (^C to kill the server)"
+    args, unknown = parser.parse_known_args()
+    unknown = json.dumps(unknown)
+    nc_hdf_path, port, payload, validate = [getattr(args, ele) for ele in vars(args)]
+    log_file_location = tempfile.mkstemp(prefix='dmrpp-generator-')[1]
+    print(f'Log file: {log_file_location}')
 
-    # Counting number of files to estimate the work
-    _, _, files = next(os.walk(nc_hdf_path))
-    # Remove dmrpp suffix from the list of files
-    [files.remove(file_) for file_ in files[:] if file_.endswith('.dmrpp')]
-    _, log_file_location = tempfile.mkstemp(prefix="dmrpp-generator-")
-    message_visit_server = "" if no_need_validation else f"Results served ( 🌎 ):\t{visit_link_path_message}"
+    if validate:
+        dmrrpp_service = ''
+        visit_link_path_message = f'http://localhost:{port}/opendap (^C to kill the server)'
+        message_visit_server = f'Results served at : {visit_link_path_message}'
+        print(message_visit_server)
+    else:
+        dmrrpp_service = 'dmrpp'
+
     try:
-        p_1 = Process(target=progress_bar, args=(len(files),))
-        p_2 = Process(target=run_docker_compose, args=(payload, nc_hdf_path, port, dmrrpp_service, log_file_location))
-        p_1.start()
-        p_2.start()
-        p_1.join()
-        print(f"{message_visit_server}\nLogs are located here (🪵 ):\t{log_file_location}")
-        p_2.join()
+        docker_compose = Process(
+            target=run_docker_compose,
+            args=(payload, unknown, nc_hdf_path, port, dmrrpp_service, log_file_location)
+        )
+        docker_compose.start()
+        docker_compose.join()
     except KeyboardInterrupt:
-        print("Shutting down the server...")
-        p_1 = Process(target=progress_bar, args=(3, "Progress", 'Complete', 10, '💀', '🔥',))
-        p_1.start()
-        p_1.join()
+        print("\nShutting down the server...")
 
 
 if __name__ == "__main__":
